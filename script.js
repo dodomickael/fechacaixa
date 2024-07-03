f783f3f784fe24de7048826d5fabc4dac381272f
Douglas Mickael <161051318+dodomickael@users.noreply.github.com>
2024-07-03 17:53:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const notes10 = parseInt(document.getElementById('notes10').value) || 0;
         const notes20 = parseInt(document.getElementById('notes20').value) || 0;
         const notes50 = parseInt(document.getElementById('notes50').value) || 0;
-    
 
         const total = (notes2 * 0.05) + (notes5 * 0.10) + (notes10 * 0.25) +
-                      (notes20 * 0.5) + (notes50 * 1)
+                      (notes20 * 0.5) + (notes50 * 1);
 
-        totalValueSpan.textContent = total;
+        totalValueSpan.textContent = total.toFixed(2);
     }
 });
