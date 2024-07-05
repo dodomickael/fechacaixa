4ae51862e39126eaec94a25d58f9e4ca4fd4c81a
Douglas Mickael <161051318+dodomickael@users.noreply.github.com>
2024-07-05 16:50:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('noteForm');
     const totalValueSpan1 = document.getElementById('totalValue1');
     const totalValueSpan2 = document.getElementById('totalValue2');
+    const totalValueSpan3 = document.getElementById('totalValue3');
 
     form.addEventListener('input', updateTotal);
 
@@ -20,12 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const coins050 = parseInt(document.getElementById('coins050').value) || 0;
         const coins1 = parseInt(document.getElementById('coins1').value) || 0;
 
-        const total = (notes2 * 2) + (notes5 * 5) + (notes10 * 10) +
-                      (notes20 * 20) + (notes50 * 50) + (notes100 * 100) +
-                      (notes200 * 200) + (coins005 * 0.05) + (coins010 * 0.10) +
-                      (coins025 * 0.25) + (coins050 * 0.50) + (coins1 * 1);
+        const coinssobrando = parseFloat(document.getElementById('coinssobrando').value) || 0;
 
-        totalValueSpan1.textContent = total.toFixed(2);
-        totalValueSpan2.textContent = total.toFixed(2);
+        const totalNotas = (notes2 * 2) + (notes5 * 5) + (notes10 * 10) +
+                      (notes20 * 20) + (notes50 * 50) + (notes100 * 100) +
+                      (notes200 * 200);
+
+        const totalCoins = + (coins005 * 0.05) + (coins010 * 0.10) +
+        (coins025 * 0.25) + (coins050 * 0.50) + (coins1 * 1);
+
+        const total = totalCoins + totalNotas + coinssobrando;
+
+        totalValueSpan1.textContent = totalNotas.toFixed(2);
+        totalValueSpan2.textContent = totalCoins.toFixed(2);
+        totalValueSpan3.textContent = total.toFixed(2);
     }
 });
